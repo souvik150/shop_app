4abf04193fefd213adf3e86c021cd5dbc1e4284f
Souvik Mukherjee <souvikmukherjee150@gmail.com>
2022-12-06 15:47:26 +0530

    feat: adds https reqs

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
 class Product with ChangeNotifier{
   final String id;
@@ -17,8 +19,22 @@ class Product with ChangeNotifier{
     this.isFavorite = false
   });
 
-  void toggleFavStatus(){
+  void toggleFavStatus() async {
+    final oldStatus = isFavorite;
     isFavorite =! isFavorite;
     notifyListeners();
+    final url = Uri.parse('https://flutter-http-2eaed-default-rtdb.firebaseio.com/products/${id}.json');
+    try {
+      final response = await http.patch(url, body: json.encode({
+          'isFavorite': isFavorite,
+        }),
+      );
+      if(response.statusCode >= 400) {
+        isFavorite = oldStatus;
+        notifyListeners();
+      }
+    } catch (error) {
+        print(error);
+    }
   }
 }
--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -28,19 +28,7 @@ class CartScreen extends StatelessWidget {
                   label: Text('\$${cart.totalAmount.toStringAsFixed(2)}'),
                   backgroundColor: Theme.of(context).primaryColor,
                 ),
-                TextButton(
-                  child: Text(
-                    "ORDER NOW",
-                    style: TextStyle(color: Theme.of(context).primaryColor),
-                  ),
-                  onPressed: () {
-                    Provider.of<Orders>(context, listen: false).addOrder(
-                      cart.items.values.toList(),
-                      cart.totalAmount,
-                    );
-                    cart.clear();
-                  },
-                )
+                OrderButton(cart: cart)
               ],
             )),
           ),
@@ -57,6 +45,38 @@ class CartScreen extends StatelessWidget {
           ))
         ],
       ),
+    );
+  }
+}
+
+class OrderButton extends StatefulWidget {
+  const OrderButton({
+    Key key,
+    @required this.cart,
+  }) : super(key: key);
+
+  final Cart cart;
+
+  @override
+  State<OrderButton> createState() => _OrderButtonState();
+}
+
+class _OrderButtonState extends State<OrderButton> {
+  var _isLoading = false;
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      child: Text(
+        "ORDER NOW",
+        style: TextStyle(color: Theme.of(context).primaryColor),
+      ),
+      onPressed: widget.cart.totalAmount <= 0 ? null : () async {
+        await Provider.of<Orders>(context, listen: false).addOrder(
+          widget.cart.items.values.toList(),
+          widget.cart.totalAmount,
+        );
+        widget.cart.clear();
+      },
     );
   }
 }
